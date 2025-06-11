8b5a53f14427788266a17e626b74a2e9de417644
Le Thanh <lethanh.000113@gmail.com>
2025-06-11 13:45:03 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-Resume/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
